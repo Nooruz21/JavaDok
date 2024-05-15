da143e965ebb59155e954f8c8a0ef60ea24ca39b
Nooruz21 <nooruz21n@gmail.com>
2024-05-15 13:31:02 +0600

    Initial commit

---
+++ app/src/main/java/com/awb/javadok/test.java
@@ -0,0 +1,7 @@
+package com.awb.javadok;
+
+/**
+ * it is only for java classes
+ */
+public class test {
+}
---
+++ documentation/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.awb.javadok","c":"test","l":"test()","u":"%3Cinit%3E()"}];updateSearchResults();
